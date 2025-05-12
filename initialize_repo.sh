8947ee7dfad41da17da9140565139ba3d80e0824
RubenPX <rubenmarchante@gmail.com>
2025-05-13 01:51:42 +0200

    DB initialize

--- initialize_repo.sh
+++ initialize_repo.sh
@@ -1,7 +1,25 @@
 # /bin/bash
 
-# tested in mcr.microsoft.com/devcontainers/javascript-node:1-22-bookworm
-# # Features ghcr.io/itsmechlark/features/redis-server
+# Build Comty DB user
+# Configura tus variables
+DB_NAME="comty"
+DB_USER="comtyuser"
+DB_PASS="tusegura_contraseña"
+
+# Ejecutar comandos en MongoDB usando mongosh
+mongosh <<EOF
+use $DB_NAME
+db.createUser({
+  user: "$DB_USER",
+  pwd: "$DB_PASS",
+  roles: [
+    { role: "readWrite", db: "$DB_NAME" },
+    { role: "dbAdmin", db: "$DB_NAME" }
+  ]
+});
+EOF
+
+# Clone and patch comty
 
 bash -i -c 'nvm install --lts && nvm use --lts'
 git clone --recurse-submodules https://github.com/ragestudio/comty && cd comty
